ed97aac3060023965002d86b6e29c9933f804f40
jessicaqiu <>
2023-04-13 14:47:48 -0700

    test lambda func

---
+++ lambda_functions/what_time.js
@@ -0,0 +1,8 @@
+// test func
+module.exports.handler = async (event, context) => {
+    const unixTime = Math.floor(Date.now() / 1000);
+    return {
+      statusCode: 200,
+      body: `The Unix time is ${unixTime}`,
+    };
+};
